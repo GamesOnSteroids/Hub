e873b83b0c32624aa2ad0421a8553dca65bf20d2
Filip Buchta <filip.buchta@embedit.cz>
2016-02-24 12:59:38 +0100

    first test

---
+++ app/games/mahjong/Table.test.ts
@@ -0,0 +1,27 @@
+namespace Mahjong.Test {
+    "use strict";
+
+    describe("chi tests", () => {
+
+        beforeEach(() => {
+            //spyOn(hand, "someMethod").andCallThrough();
+        });
+
+        afterEach(() => {
+
+        });
+
+        it("man 123", () => {
+            let table = new Table();
+            let myHand = new Hand();
+            myHand.tiles.push(TILE_MAP.get(TileId.Man1), TILE_MAP.get(TileId.Man2));
+            let moves = table.getAvailableMovesOnYourTurn(TileId.Man1, myHand);
+            expect(moves.length).toBe(1);
+            expect(moves[0].type).toBe(MoveType.Chi);
+            expect(moves[0].tile[0]).toBe(TileId.Man1);
+            expect(moves[0].tile[1]).toBe(TileId.Man2);
+            expect(moves[0].tile[2]).toBe(TileId.Man3);
+        });
+
+    });
+}
