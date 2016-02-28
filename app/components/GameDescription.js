7ca06324be34f93a0376ba187084761cdfd20154
Filip Buchta <filip.buchta@embedit.cz>
2016-02-28 17:31:59 +0100

    Update 2016-02-28T16:31:53.344Z

--- app/components/GameDescription.js
+++ app/components/GameDescription.js
@@ -5,9 +5,9 @@ class GameDescription extends React.Component {
         this.startGame = this.startGame.bind(this);
     }
     render() {
-        return (React.createElement("div", null, React.createElement("h3", null, this.props.game.name), React.createElement("div", null, React.createElement("img", {className: "pull-right", width: "128", src: "app/games/" + this.props.game.id + "/assets/images/logo.png"}), React.createElement("p", null, this.props.game.description), React.createElement("span", null, "Currently playing: ? games")), React.createElement("div", {className: "btn-group-vertical", role: "group"}, this.props.game.variants
+        return (React.createElement("div", {style: { padding: "5px" }}, React.createElement("h3", {style: { backgroundColor: "#eee", padding: "0" }, className: "text-center"}, this.props.game.name), React.createElement("div", null, React.createElement("div", null, React.createElement("img", {width: "128", src: "app/games/" + this.props.game.id + "/assets/images/logo.png"})), React.createElement("div", {className: "pull-right"}, React.createElement("div", {className: "btn-group-vertical", role: "group"}, this.props.game.variants
             .filter((v) => v.development != true || environment == EnvironmentType.Development)
-            .map((variant) => (React.createElement("button", {key: variant.id, type: "button", className: variant.id == "default" ? "btn btn-primary" : "btn btn-default", onClick: this.startGame.bind(this, variant)}, variant.name))))));
+            .map((variant) => (React.createElement("button", {key: variant.id, type: "button", className: variant.id == "default" ? "btn btn-primary" : "btn btn-default", onClick: this.startGame.bind(this, variant)}, variant.name)))))), React.createElement("div", {className: "row"}, React.createElement("div", {className: "col-md-12"}, React.createElement("p", null, this.props.game.description), React.createElement("span", null, "Currently playing: ? games")))));
     }
     componentDidMount() {
         if (environment == EnvironmentType.Development) {
